4e0d3a820e95f2d59da6f4d15a485376df81e71a
wbjeon2k <wbjeon2k@gmail.com>
2021-03-18 00:29:45 +0900

    debugging error on timer, thread_sleep, thread_wake

--- project1/pintos/src/devices/timer.c
+++ project1/pintos/src/devices/timer.c
@@ -173,7 +173,7 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
-  thread_tick ();
+  thread_tick(timer_ticks());
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
