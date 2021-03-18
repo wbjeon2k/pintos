c56d4d0a8376d339a829b3f6fe38fd5986bdf87d
wbjeon2k <wbjeon2k@gmail.com>
2021-03-19 05:24:32 +0900

    timer

--- project1/pintos/src/threads/thread.c
+++ project1/pintos/src/threads/thread.c
@@ -139,7 +139,9 @@ void thread_sleep(int64_t wakeuptime) {
     enum intr_level old_level;
     old_level = intr_disable();
 
-    list_insert_ordered(&sleeping_list, thread_current()->sleep_elem, comparator_sleep, NULL);
+    struct thread* t = thread_current();
+
+    list_insert_ordered(&sleeping_list, &t->sleep_elem, comparator_sleep, NULL);
     thread_block();
 
     intr_set_level(old_level);
