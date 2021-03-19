d81e60b36728ed6eda450120c5f97abf11859efe
wbjeon2k <wbjeon2k@gmail.com>
2021-03-19 14:35:05 +0900

    try priority first

--- project1/pintos/src/threads/synch.c
+++ project1/pintos/src/threads/synch.c
@@ -125,7 +125,7 @@ sema_up (struct semaphore *sema)
   old_level = intr_disable ();
 
   if (!list_empty(&sema->waiters)) {
-      list_sort(&sema->waiters, comparator_waiting);
+      list_sort(&sema->waiters, comparator_waiting, NULL);
       thread_unblock(list_entry(list_pop_back(&sema->waiters), struct thread, elem));
   }
 
