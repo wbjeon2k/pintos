a0e55060e524933f0058472e6723cae839eb3587
wbjeon2k <wbjeon2k@gmail.com>
2021-03-19 15:13:45 +0900

    back from the test

--- project1/pintos/src/threads/thread.c
+++ project1/pintos/src/threads/thread.c
@@ -401,18 +401,13 @@ thread_yield (void)
   ASSERT (!intr_context ());
 
   old_level = intr_disable ();
-  /*
+  
   if (cur != idle_thread) {
       list_insert_ordered(&ready_list, &cur->elem, comparator_priority, NULL);
   }
   
   cur->status = THREAD_READY;
-  */
-  //experimental
-  if (cur != idle_thread) {
-      cur->status = THREAD_BLOCKED;
-      thread_sleep(timer_ticks() + 1);
-  }
+
   schedule ();
   intr_set_level (old_level);
 }
