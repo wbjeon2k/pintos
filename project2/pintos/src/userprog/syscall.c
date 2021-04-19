95f9a7c715eccb82954ce89f59b94ba65e59afe7
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-04-19 23:14:15 +0900

    working on 3.2

--- project2/pintos/src/userprog/syscall.c
+++ project2/pintos/src/userprog/syscall.c
@@ -32,7 +32,13 @@ For # of arguments is
 */
 
 uint32_t* esp_offset(const struct intr_frame* f, int i) {
+    uint32_t* ret = f->esp;
+    ret += i;
+    return ret;
+}
 
+inline bool check_VA(void* ptr) {
+    return is_user_vaddr(ptr);
 }
 
 
@@ -72,39 +78,92 @@ void close(int fd);
 */
 
 
+
+/*
+For # of arguments is
+1 : argument is located in esp + 1
+2 : arguments are located in esp + 4(arg0), esp + 5(arg1)
+3 : arguments are located in esp + 5(arg0), esp + 6(arg1), esp + 7(arg2)
+*/
+
+// System calls that return a value can do so by modifying the "eax" member of struct intr_frame. 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  //printf ("system call!\n");
-  //thread_exit ();
+    //for test
+    /*
+    printf ("system call!\n");
+    thread_exit ();
+    return;
+    */
+
+    if (f == NULL) exit_impl(-1);
 
     uint32_t* esp_copy = f->esp;
     //check esp
+    if (!check_VA(esp_copy)) exit_impl(-1);
+
     uint32_t syscall_nr = *esp_copy;
+
+    if (syscall_nr == SYS_HALT) {
+        //in src/devices/shutdown
+        halt_impl();
+    }
 
     if (syscall_nr == SYS_EXIT) {
         //process_exit();
-        thread_exit();
+        if (!check_VA(esp_offset(f, 1))) exit_impl(-1);
+
+        int exit_code = *esp_offset(f, 1);
+        //const char *thread_name (void);
+        exit_impl(exit_code);
     }
 
     if (syscall_nr == SYS_WRITE) {
-        //get 3 arguments
+        //get 3 arguments int fd, const void* buffer, unsigned length
         //call write_implement
+        if (!check_VA(esp_offset(f, 5))) exit_impl(-1);
+        if (!check_VA(esp_offset(f, 6))) exit_impl(-1);
+        if (!check_VA(esp_offset(f, 7))) exit_impl(-1);
+
+        int fd = *esp_offset(f, 5);
+        void* buffer = *esp_offset(f, 6);
+        unsigned length = *esp_offset(f, 7);
+        f->eax = write_impl(fd, buffer, length);
+    }
+
+    if (syscall_nr == SYS_READ) {
+        //get 3 arguments int fd, const void* buffer, unsigned length
+        //call write_implement
+        if (!check_VA(esp_offset(f, 5))) exit_impl(-1);
+        if (!check_VA(esp_offset(f, 6))) exit_impl(-1);
+        if (!check_VA(esp_offset(f, 7))) exit_impl(-1);
+
+        int fd = *esp_offset(f, 5);
+        void* buffer = *esp_offset(f, 6);
+        unsigned length = *esp_offset(f, 7);
+        f->eax = read_impl(fd, buffer, length);
     }
 
     if (syscall_nr == SYS_WAIT) {
         //get tid
-        tid_t tid = 0;
-        process_wait(tid);
+
+        if (!check_VA(esp_offset(f, 1))) exit_impl(-1);
+
+        tid_t tid = *esp_offset(f, 1);
+        
+        f->eax = process_wait(tid);
     }
 }
 
 void halt_impl(void) {
-
+    //in src/devices/shutdown
+    shutdown_power_off();
 }
 
 void exit_impl(int status) {
-
+    printf("%s: exit(%d)\n", thread_name(), exit_code);
+    thread_exit();
 }
 
 pid_t exec_impl(const char* file) {
@@ -131,12 +190,27 @@ int filesize(int fd) {
 
 }
 
-int read(int fd, void* buffer, unsigned length) {
+int read_impl(int fd, void* buffer, unsigned length) {
+    if (fd == 0) {
+        int i = 0;
+        int cnt = 0;
+        for (i = 0; i < length; ++i) {
+            //uint8_t input_getc(void)
+            if (input_getc() == 0) break;
 
+            ++cnt;
+        }
+
+        return cnt;
+    }
 }
 
 int write_impl(int fd, const void* buffer, unsigned length) {
-
+    if (fd == 1) {
+        //fd 1, the system console
+        putbuf(buffer, length);
+        return length;
+    }
 }
 
 void seek(int fd, unsigned position) {
