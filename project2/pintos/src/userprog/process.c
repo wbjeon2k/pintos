f85c92a0db3b3aa1d4851bc4bad529877b34c2e7
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-04-25 00:05:40 +0900

    argument passing

--- project2/pintos/src/userprog/process.c
+++ project2/pintos/src/userprog/process.c
@@ -83,7 +83,8 @@ process_execute (const char *command)
 
   checkpoint(3);
 
-  file_name = strtok_r(cmd_copy, " ", &tmp_ptr);
+  file_name = strtok_r(&cmd_copy, " ", &tmp_ptr);
+  printf("fliename %s", file_name);
 
   checkpoint(4);
 
