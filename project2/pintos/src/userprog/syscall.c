b9c02756aba7a4c04ebdafdce75149f2e9a23d0a
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-04-26 19:15:40 +0900

    try rox- tests

--- project2/pintos/src/userprog/syscall.c
+++ project2/pintos/src/userprog/syscall.c
@@ -31,6 +31,14 @@ void close(int);
 
 struct lock file_lock;
 
+/* An open file. */
+struct file
+{
+    struct inode* inode;        /* File's inode. */
+    off_t pos;                  /* Current position. */
+    bool deny_write;            /* Has file_deny_write() been called? */
+};
+
 void
 syscall_init (void) 
 {
