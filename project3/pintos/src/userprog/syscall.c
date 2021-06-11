9659af1c371e301b1b9ac3823c4fc8960809fe4b
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-06-12 00:08:17 +0900

    20210611 D-2 sptht

--- project3/pintos/src/userprog/syscall.c
+++ project3/pintos/src/userprog/syscall.c
@@ -78,9 +78,9 @@ inline bool check_VA(void* ptr) {
     if(!is_user_vaddr(ptr)) return false;
     if (ptr == NULL) return false;
     if (ptr < 0x08048000) return false;
-    //struct thread* cur = thread_current();
+    struct thread* cur = thread_current();
 
-    //if (pagedir_get_page(cur->pagedir, ptr) == NULL) return false;
+    if (pagedir_get_page(cur->pagedir, ptr) == NULL) return false;
     /*
     void* ptr_page = (void*)pg_round_down(ptr);
     struct SPTE* tmp = find_SPTE(cur->sptht, ptr_page);
