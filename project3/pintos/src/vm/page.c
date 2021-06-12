d20aeca8fd00f36f368405e4589649a63fe6cb0a
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-06-13 01:57:21 +0900

    20210611 D-2 mmap passing more test

--- project3/pintos/src/vm/page.c
+++ project3/pintos/src/vm/page.c
@@ -154,6 +154,7 @@ bool enroll_spte_zeropage(struct SPTHT* sptht, void* VA) {
 	new_spte->PA = NULL;
 	new_spte->isValid = false;
 	new_spte->spte_flags = SPTE_ZERO;
+	new_spte->writable = true;
 
 	if (insert_SPTE(sptht, new_spte)) return true;
 	else {
@@ -275,17 +276,6 @@ bool load_on_pagefault(struct SPTHT* sptht, void* VA, uint32_t* pagedir) {
 	if (spte->spte_flags == SPTE_ZERO) {
 		//zero page
 		//already zeroed at frame_alloc(PAL_USER | PAL_ZERO). just continue
-		if (pagedir_set_page(pagedir, spte->VA, get_frame, spte->writable) == false) {
-			frame_free(get_frame);
-			return false;
-		}
-
-		//8. set SPTE valid --> make it on frame
-		spte->isValid = true;
-		spte->PA = get_frame;
-
-		return true;
-
 		if (pagedir_set_page(pagedir, spte->VA, get_frame, spte->writable) == false) {
 			frame_free(get_frame);
 			return false;
